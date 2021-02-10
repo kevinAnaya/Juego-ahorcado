1946988ae003ef45802e541bfcb34a271bf43b09
Kevin Anaya <kevinanaya278@gmail.com>
2021-02-10 12:54:34 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ahorcado';
+  
+  palabra = 'COCHERA';
+  palabraOculta = '';
+  intentos = 0;
+
+  gano = false;
+  perdio = false;
+  
+  letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+            'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+
+  constructor() {
+    this.palabraOculta = ('_ ').repeat(this.palabra.length);
+  }
+
+  comprobar(letra){
+    this.existeLetra(letra);
+
+    const palabraOcultaArr = this.palabraOculta.split(' ');
+
+    for(let i = 0; i <= this.palabra.length; i++){
+      if(this.palabra[i] === letra){
+        palabraOcultaArr[i] = letra;
+      }
+    } 
+
+    this.palabraOculta = palabraOcultaArr.join(' ');
+    this.comprobarGane();
+    
+    
+  }
+
+  comprobarGane(){
+
+    const palabraArr = this.palabraOculta.split(' ');
+    const palabraEvaluar = palabraArr.join('');
+    
+    if(palabraEvaluar === this.palabra){
+      this.gano = true;
+    }
+    
+    if(this.intentos >= 9){
+      this.perdio = true;
+    }
+    
+
+  }
+
+  existeLetra(letra){
+     if(this.palabra.indexOf(letra) >= 0){
+       //encontró la letra
+     }else{
+       this.intentos ++;
+     }
+  }
 }
